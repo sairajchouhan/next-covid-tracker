c29ca510896e370f3918032d6aa02d0ffe977e2c
sairaj <sairaj2119@gmail.com>
2021-04-28 19:27:45 +0530

    add select input

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,9 @@ import format from 'date-fns/format';
 import HomeCard from '../components/HomeCard';
 import { GetServerSideProps, GetStaticProps } from 'next';
 
-const Home = ({ data }) => {
+const Home = ({ data, countries }) => {
   const { Global: global } = data;
-  console.log(global);
-  const parsed = parseISO(global.Date);
-  const res = format(parsed, 'qo MMMM yyyy');
+  console.log(countries);
 
   return (
     <div className="">
@@ -32,14 +30,39 @@ const Home = ({ data }) => {
           total={global.TotalDeaths}
         />
       </div>
+      <div className="container w-5/6 mx-auto text-center my-3 mt-20">
+        <p className="mb-2 text-gray-900">Select a Country</p>
+
+        <select
+          name="Country"
+          id="country"
+          placeholder="Choose a country"
+          className="w-80 rounded-md h-9 px-1 
+          border border-gray-400
+          focus:ring-2 focus:ring-blue-600 focus:outline-none
+          shadow-lg
+          "
+        >
+          <option value="">Global</option>
+          {countries.map((c: any) => (
+            <option value={c.Slug} key={c.Slug}>
+              {c.Country}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const { data } = await axios.get('https://api.covid19api.com/summary');
+  const { data: countries } = await axios.get(
+    'https://api.covid19api.com/countries'
+  );
+
   return {
-    props: { data },
+    props: { data, countries },
   };
 };
 
